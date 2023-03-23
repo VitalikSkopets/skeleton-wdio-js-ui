3e46fafb507bdcdadc7dd1769d7319e330ffe8b4
Vitaly Skopets <vitaly.skopets@gitrexgroup.com>
2023-03-23 19:19:13 +0400

    Homework 1

--- test/specs/spec.login.js
+++ test/specs/spec.login.js
@@ -9,6 +9,14 @@ describe('Login', () => {
 
         await expect($('h2')).toHaveText('Secure Area');
     });
+
+    it('Invalid password based authentication', async () => {
+        await browser.url('https://the-internet.herokuapp.com/login');
+
+        await $('#username').setValue('tomsmith');
+        await $('#password').setValue('invalidPassword');
+        await $('button[type="submit"]').click();
+
+        await expect($('.error')).toHaveTextContaining('Your password is invalid!');
+    });
 })
-
-
